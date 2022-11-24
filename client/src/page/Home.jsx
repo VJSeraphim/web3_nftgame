a3c225414e9aa453e8a8a1be71022b0297e2bfa5
VJS <minsung_96@naver.com>
2022-11-24 23:53:04 +0900

    Wallet Connection to the Frontend

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import { PageHOC } from '../components';
+import { PageHOC, CustomInput } from '../components';
+import { useGlobalContext } from '../context';
 
 const Home = () => {
+  const { contract, walletAddress } = useGlobalContext()
+
   return (
-    <div>
+    <div className="flex flex-col">
+      <CustomInput 
       
+      />
     </div>
   )
 };
