b8be1fd0eb0815e15cebc056f7ca001ee398e138
VJS <minsung_96@naver.com>
2022-12-04 23:59:43 +0900

    Connecting Frontend to ethereum

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -3,19 +3,27 @@ import { ethers } from 'ethers'
 import Web3Modal from 'web3modal'
 import { useNavigate } from 'react-router-dom'
 
+import { ABI, ADDRESS } from '../contract'
+
 const GlobalContext = createContext()
 
 export const GlobalContextProvider = ({ children }) => {
     const [walletAddress, setWalletAddress] = useState('')
+    const [provider, setProvider] = useState('')
+    const [contract, setContract] = useState('')
 
     const updateCurrentWalletAddress = async () => {
         const accounts = await window.ethereum.request({
             method: 'eth_requestAccounts'
         })
+
+        if(accounts) setWalletAddress(accounts[0])
     }
 
     useEffect(() => {
       updateCurrentWalletAddress()
+
+      window.ethereum.on('accountsChanged', updateCurrentWalletAddress)
     }, [])
 
     useEffect(() => {
@@ -24,7 +32,10 @@ export const GlobalContextProvider = ({ children }) => {
         const connection = await web3Modal.connect()
         const newProvider = new ethers.providers.Web3Provider(connection)
         const signer = newProvider.getSigner()
-        const newContract = new ethers.Contract()
+        const newContract = new ethers.Contract(ADDRESS, ABI, signer)
+
+        setProvider(newProvider)
+        setContract(newContract)
       }
 
       setSmartContractAndProvider()
