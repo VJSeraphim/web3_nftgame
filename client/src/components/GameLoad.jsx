8f49a472e07f577e908e16a30514d2695d35aeae
VJS <minsung_96@naver.com>
2022-12-11 23:57:48 +0900

    Versus Page

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -19,6 +19,31 @@ const GameLoad = () => {
                     restStyles="mt-6"
                 />
             </div>
+            <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+                <h1 className={`${styles.headText} text-center`}>
+                    Waiting for a <br /> worthy opponent...
+                </h1>
+                <p className={styles.gameLoadText}>
+                    Protip : while you're waiting, choose your preferred battleground.
+                </p>
+                <div className={styles.gameLoadPlayersBox}>
+                    <div className={`${styles.flexCenter} flex-col`}>
+                        <img src={player01} className={styles.gameLoadPlayerImg} />
+                        <p className={styles.gameLoadPlayerText}>
+                            {walletAddress.slice(0, 30)}
+                        </p>
+                    </div>
+                    <h2 className={styles.gameLoadVS}>
+                        VS
+                    </h2>
+                    <div className={`${styles.flexCenter} flex-col`}>
+                        <img src={player02} className={styles.gameLoadPlayerImg} />
+                        <p className={styles.gameLoadPlayerText}>
+                            ????
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
